3cd1c3100bc4f2ac82ee6670e6a0d17adcfc0b06
Dan H <9552296+anzumura@users.noreply.github.com>
2023-11-18 15:59:01 -0500

    reformat comments

--- src/main/scala/quiz/KanjiType.scala
+++ src/main/scala/quiz/KanjiType.scala
@@ -12,8 +12,7 @@ package quiz
  * Frequency: 124 from 'frequency.txt' that aren't one of the above types
  * Extra: loaded from 'extra.txt' (file doesn't contain above types)
  * Kentei: loaded from 'kentei/' directory and not one of the above types
- * Ucd: loaded from 'ucd.txt' and not one of the above types
- */
+ * Ucd: loaded from 'ucd.txt' and not one of the above types */
 object KanjiType extends Enumeration {
   type KanjiType = Value
   val Jouyou, Jinmei, LinkedJinmei, LinkedOld, Frequency, Extra, Kentei, Ucd
@@ -21,8 +20,7 @@ object KanjiType extends Enumeration {
 }
 
 /**
- * represents the official school grade for all Jouyou Kanji
- */
+ * represents the official school grade for all Jouyou Kanji */
 object Grade extends Enumeration {
   type Grade = Value
   val G1, G2, G3, G4, G5, G6, S, None = Value
@@ -30,8 +28,7 @@ object Grade extends Enumeration {
 
 /**
  * JLPT (Japanese Language Proficiency Test) Levels covers 2,222 total Kanji
- * (including 1,971 Jouyou and 251 Jinmei)
- */
+ * (including 1,971 Jouyou and 251 Jinmei) */
 object Level extends Enumeration {
   type Level = Value
   val N5, N4, N3, N2, N1, None = Value
@@ -40,8 +37,7 @@ object Level extends Enumeration {
 /**
  * Kanji Kentei (漢字検定) Kyū (級), K = Kanken (漢検), J=Jun (準)
  *
- * @see <a href="https://en.wikipedia.org/wiki/Kanji_Kentei"></a>
- */
+ * @see <a href="https://en.wikipedia.org/wiki/Kanji_Kentei"></a> */
 object Kyu extends Enumeration {
   type Kyu = Value
   val K10, K9, K8, K7, K6, K5, K4, K3, KJ2, K2, KJ1, K1, None = Value
@@ -55,8 +51,7 @@ object Kyu extends Enumeration {
  * Moved: 5 Kanji - moved out of Jouyou into Jinmei
  * Simple: 2 Kanji - simplified (表外漢字字体表の簡易慣用字体)
  * Other: 26 Kanji - reason listed as その他
- * None: not a Jinmei type Kanji
- */
+ * None: not a Jinmei type Kanji */
 object JinmeiReason extends Enumeration {
   type JinmeiReason = Value
   val Names, Print, Variant, Moved, Simple, Other, None = Value
