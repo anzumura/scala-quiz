9bb22ef39b6d562db02dd6ed506493d93a2bf24c
Dan H <9552296+anzumura@users.noreply.github.com>
2023-11-22 18:14:37 -0500

    implement get function

--- src/main/scala/quiz/Choice.scala
+++ src/main/scala/quiz/Choice.scala
@@ -2,21 +2,23 @@ package quiz
 
 import quiz.Choice._
 
-import java.io.{InputStream, PrintStream}
+import java.io.{BufferedReader, InputStream, InputStreamReader, PrintStream}
+import scala.annotation.tailrec
+import scala.collection.immutable.TreeMap
+import scala.language.implicitConversions
 
-class Choice private (private val os: PrintStream = System.out,
-    private val is: Option[InputStream] = None,
-    private var quitIn: Option[Char] = None,
-    var quitDescription: String = DefaultQuitDescription) {
-  private var _quit: Option[Char] = None
+class Choice private (private var _quit: Option[Char] = None,
+    var quitDescription: String = DefaultQuitDescription,
+    val is: InputStream = System.in, private val os: PrintStream = System.out) {
+  // call setQuit to force validation upon construction
+  _quit.foreach(setQuit)
+  private val reader = new BufferedReader(new InputStreamReader(is))
 
   def quit: Option[Char] = _quit
   def quit_=(q: Option[Char]): Unit = {
     q.foreach(x => checkChoice(x, "quit option"))
     _quit = q
   }
-  // use public setter for initializing 'quit' to get consistent validation
-  quit = quitIn
 
   // helper methods to avoid using Option
   def isQuit(q: Char): Boolean = quit.contains(q)
@@ -28,8 +30,20 @@ class Choice private (private val os: PrintStream = System.out,
     quitDescription = d
   }
 
-  def get(msg: String, useQuit: Boolean, choices: Map[Char, String],
-      defaultChoice: Option[Char] = None): Char = {
+  /**
+   * read in a choice from `is` provided in the ctor
+   * @param msg beginning part of prompt message written to `os` specified in
+   *            the ctor, the rest of the prompt message shows the choices
+   * @param useQuit if 'QuitOff' then 'quit option' is not included in choices
+   * @param choices set of choices the user must choose from
+   * @param defaultChoice optional default choice (for just pressing return)
+   * @return one of the choices from `choices` or possibly the 'quit option'
+   * @throws DomainError if `def` is provided, but it's not in `choicesIn`
+   * @throws DomainError if 'quit option' has been set and is also in `choices`
+   * @throws DomainError if any choice in `choices` is not printable Ascii
+   */
+  def get(msg: String, choices: Choices, defaultChoice: Option[Char] = None,
+      useQuit: UseQuit = QuitOn): Char = {
     val c = quit.map(q =>
       if (useQuit) {
         if (choices.contains(q)) error(s"quit option '$q' already in choices")
@@ -37,14 +51,76 @@ class Choice private (private val os: PrintStream = System.out,
       } else choices
     ).getOrElse(choices)
     if (c.isEmpty) error("must specify at least one choice")
-    val prompt = add(msg, choices, defaultChoice)
-    os.print(prompt)
-    os.flush()
-    'a'
+    getChoice(getPrompt(msg, c, defaultChoice), c, defaultChoice)
   }
 
-  private def add(msg: String, choices: Map[Char, String], d: Option[Char]) = {
-    val prompt = new StringBuilder(if (msg.isEmpty) "(" else " (")
+  // 'get' overloads to avoid forcing the caller to use Option[defaultChoice)
+  def get(msg: String, choices: Choices, defaultChoice: Char): Char =
+    get(msg, choices, Option(defaultChoice))
+  def get(msg: String, choices: Choices, useQuit: UseQuit): Char =
+    get(msg, choices, None, useQuit)
+  def get(msg: String, choices: Choices, defaultChoice: Char, useQuit: UseQuit)
+      : Char = get(msg, choices, Option(defaultChoice), useQuit)
+  // 'get' overloads that don't require 'msg'
+  def get(choices: Choices): Char = get("", choices, None)
+  def get(choices: Choices, defaultChoice: Char): Char =
+    get("", choices, Option(defaultChoice))
+  def get(choices: Choices, useQuit: UseQuit): Char =
+    get("", choices, None, useQuit)
+  def get(choices: Choices, defaultChoice: Char, useQuit: UseQuit): Char =
+    get("", choices, Option(defaultChoice), useQuit)
+
+  @tailrec
+  private def getChoice(prompt: String, choices: Choices,
+      defaultChoice: Option[Char] = None): Char = {
+    (read(prompt), defaultChoice) match {
+      case (Some(choice), _) if choices.contains(choice) => choice
+      case (None, Some(choice)) => choice
+      case _ => getChoice(prompt, choices, defaultChoice)
+    }
+  }
+
+  @tailrec
+  private def read(prompt: String): Option[Char] = {
+    os.print(prompt)
+    os.flush()
+    reader.readLine() match {
+      case line if line.isEmpty => None
+      case line if line.length == 1 => Option(line(0))
+      case _ => read(prompt)
+    }
+  }
+
+  private def getPrompt(msg: String, choices: Choices, d: Option[Char]) = {
+    val prompt = new StringBuilder(if (msg.isEmpty) "(" else s"$msg (")
+    var rangeStart = Option.empty[Char]
+    var prevChoice: Char = 0
+    val completeRange = () =>
+      rangeStart.foreach(c => if (c != prevChoice) prompt ++= s"-$prevChoice")
+    // use TreeMap to traverse entries in alphabetical order
+    (choices to TreeMap).foreach { case (choice, description) =>
+      checkChoice(choice, "option")
+      if (description.isEmpty) {
+        if (rangeStart.isEmpty) {
+          if (prevChoice > 0) prompt ++= ", "
+          prompt += choice
+          rangeStart = Option(choice) // start new range
+        } else if (choice - prevChoice > 1) {
+          completeRange() // complete range if jump in option values is > 1
+          prompt ++= s", $choice"
+          rangeStart = Option(choice)
+        } // continue processing consecutive values (so don't update rangeStart)
+      } else {
+        if (rangeStart.nonEmpty) {
+          completeRange()
+          rangeStart = None
+        }
+        if (prevChoice > 0) prompt ++= ", "
+        prompt ++= s"$choice=$description"
+      }
+      prevChoice = choice
+    }
+    completeRange()
     d match {
       case Some(s) =>
         if (!choices.contains(s)) error(s"default option '$s' not in choices")
@@ -53,20 +129,28 @@ class Choice private (private val os: PrintStream = System.out,
     }
     prompt.result()
   }
-
-  private def error(msg: String) = throw DomainException(msg)
 }
 
 object Choice {
+  type Choices = Map[Char, String]
   val DefaultQuitDescription = "quit"
+
+  sealed trait UseQuit
+  case object QuitOn extends UseQuit
+  case object QuitOff extends UseQuit
+  object UseQuit {
+    implicit def toBoolean(x: UseQuit): Boolean = x eq QuitOn
+  }
 
   def apply(): Choice = new Choice
   def apply(q: Char, d: String = DefaultQuitDescription): Choice =
-    new Choice(quitIn = Option(q), quitDescription = d)
+    new Choice(Option(q), d)
+  // used by test code
+  def apply(is: InputStream, os: PrintStream): Choice =
+    new Choice(is = is, os = os)
 
   private def checkChoice(c: Char, msg: String): Unit = {
-    if (c < ' ' || c > '~')
-      error(s"invalid $msg: 0x" + c.toInt.toHexString)
+    if (c < ' ' || c > '~') error(s"invalid $msg: '0x${c.toInt.toHexString}'")
   }
 
   private def error(msg: String) = throw DomainException(msg)
--- src/test/scala/quiz/ChoiceTest.scala
+++ src/test/scala/quiz/ChoiceTest.scala
@@ -1,7 +1,9 @@
 package quiz
 
 import org.scalatest.freespec.AnyFreeSpec
-import quiz.Choice.DefaultQuitDescription
+import quiz.Choice._
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintStream}
 
 class ChoiceTest extends AnyFreeSpec {
   "create with default options" in {
@@ -26,8 +28,8 @@ class ChoiceTest extends AnyFreeSpec {
   }
 
   "create with invalid quit option" in {
-    val e = intercept[DomainException] { Choice(12) }
-    assert("invalid quit option: 0xc" == e.getMessage)
+    val e = intercept[DomainException] { Choice(12: Char) }
+    assert("invalid quit option: '0xc'" == e.getMessage)
   }
 
   "update quit value" in {
@@ -43,6 +45,130 @@ class ChoiceTest extends AnyFreeSpec {
   "update to invalid quit option" in {
     val c = Choice()
     val e = intercept[DomainException] { c.quit = Option(13) }
-    assert("invalid quit option: 0xd" == e.getMessage)
+    assert("invalid quit option: '0xd'" == e.getMessage)
+  }
+
+  "update quit and quit description" in {
+    val expected = 'q'
+    val expectedDesc = "my quit"
+    val c = Choice()
+    c.setQuit(expected, expectedDesc)
+    assert(c.isQuit(expected))
+    assert(expectedDesc == c.quitDescription)
+  }
+
+  "get" - {
+    "one choice" in {
+      val (c, os) = create("a")
+      val msg = "my choice"
+      assert('a' == c.get(msg, Map(('a', "first"))))
+      assert(s"$msg (a=first): " == os.toString)
+    }
+
+    "choice with no message" in {
+      val (c, os) = create("a")
+      assert('a' == c.get(Map(('a', ""), ('b', "second"))))
+      assert("(a, b=second): " == os.toString)
+    }
+
+    "quit choice" in {
+      val expected = 'q'
+      val (c, os) = create(s"$expected")
+      c.setQuit(expected)
+      assert(expected == c.get(Map()))
+      assert(s"($expected=quit): " == os.toString)
+    }
+
+    "choice with no description" in {
+      val (c, os) = create("a")
+      assert('a' == c.get(Map(('a', ""), ('b', "second"))))
+      assert("(a, b=second): " == os.toString)
+    }
+
+    "multiple choices are sorted" in {
+      val (c, os) = create("a")
+      c.get(Map(('c', "third"), ('b', "second"), ('a', "first")))
+      assert("(a=first, b=second, c=third): " == os.toString)
+    }
+
+    "multiple consecutive choices without descriptions use '-'" in {
+      val (c, os) = create("e")
+      c.get(Map(('e', "E"), ('f', ""), ('g', ""), ('h', ""), ('j', "")))
+      assert("(e=E, f-h, j): " == os.toString)
+    }
+
+    "with default" in {
+      val (c, os) = create("a")
+      val msg = "my default"
+      assert('a' == c.get(msg, Map(('a', "first")), 'a'))
+      assert(s"$msg (a=first) def 'a': " == os.toString)
+    }
+
+    "empty input selects default" in {
+      val (c, os) = create()
+      assert('b' == c.get(Map(('a', "first"), ('b', "second")), 'b'))
+      assert("(a=first, b=second) def 'b': " == os.toString)
+    }
+
+    "quit can be the default" in {
+      val (c, os) = create("x")
+      c.setQuit('x', "my quit")
+      assert('x' == c.get(Map(('a', "first")), 'x'))
+      assert("(a=first, x=my quit) def 'x': " == os.toString)
+    }
+
+    "suppress 'quit' option" in {
+      val (c, os) = create("a")
+      c.setQuit('q')
+      assert('a' == c.get("my msg", Map(('a', "")), QuitOff))
+      assert("my msg (a): " == os.toString())
+    }
+
+    "suppress 'quit' option and include same choice" in {
+      val (c, os) = create("q")
+      c.setQuit('q')
+      assert('q' == c.get(Map(('q', "Q")), QuitOff))
+      assert("(q=Q): " == os.toString())
+    }
+
+    "suppress 'quit' option with default" in {
+      val (c, os) = create()
+      c.setQuit('q')
+      assert('a' == c.get(Map(('a', "")), 'a', QuitOff))
+      assert("(a) def 'a': " == os.toString())
+    }
+  }
+
+  "get errors" - {
+    "no choices specified" in {
+      val c = Choice()
+      val e = intercept[DomainException] { c.get(Map()) }
+      assert("must specify at least one choice" == e.getMessage)
+    }
+
+    "invalid choice" in {
+      val c = Choice()
+      val e = intercept[DomainException] { c.get(Map(('a', "A"), (14, "bad"))) }
+      assert("invalid option: '0xe'" == e.getMessage)
+    }
+
+    "default option not in choices" in {
+      val c = Choice()
+      val e = intercept[DomainException] { c.get(Map(('a', "")), 'b') }
+      assert("default option 'b' not in choices" == e.getMessage)
+    }
+
+    "quit option in choices" in {
+      val c = Choice()
+      c.setQuit('a')
+      val e = intercept[DomainException] { c.get(Map(('a', ""))) }
+      assert("quit option 'a' already in choices" == e.getMessage)
+    }
+  }
+
+  private def create(input: String = "") = {
+    val is = new ByteArrayInputStream((input + '\n').getBytes())
+    val os = new ByteArrayOutputStream()
+    (Choice(is, new PrintStream(os)), os)
   }
 }
