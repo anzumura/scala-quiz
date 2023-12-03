f1dfb7d316ffd8ebbcfd06ce593ed5e36ff09515
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-03 11:46:16 -0500

    refactor frequency to use a local map instead of holding on to a ListFile

--- src/main/scala/quiz/KanjiData.scala
+++ src/main/scala/quiz/KanjiData.scala
@@ -8,7 +8,10 @@ import scala.reflect.ClassTag
 class KanjiData protected (val path: Path) {
   private lazy val levels = load(Level)
   private lazy val kyus = load(Kyu)
-  private lazy val frequencies = KanjiListFile(textFile(path, "frequency"))
+  private lazy val frequencies =
+    KanjiListFile(textFile(path, "frequency")).indices.map { case (k, v) =>
+      (k, v + 1)
+    }
 
   /** JLPT level of `s` or "None" if it doesn't have a level */
   def level(s: String): Level.Value = levels.getOrElse(s, Level.None)
@@ -17,7 +20,7 @@ class KanjiData protected (val path: Path) {
   def kyu(s: String): Kyu.Value = kyus.getOrElse(s, Kyu.None)
 
   /** frequency of `s` starting at 1 or 0 if it doesn't have a frequency */
-  def frequency(s: String): Int = frequencies.index(s).map(_ + 1).getOrElse(0)
+  def frequency(s: String): Int = frequencies.getOrElse(s, 0)
 
   private def load[T <: EnumWithNone: ClassTag](e: T): Map[String, T#Value] = {
     e.defined.map(EnumListFile(path.resolve(e.toString), _)).flatMap(f =>
