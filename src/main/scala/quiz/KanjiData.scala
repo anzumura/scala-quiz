fe7ff394d69d4578d299c26f825caccf3733b71e
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-02 22:42:24 -0500

    made load function more generic

--- src/main/scala/quiz/KanjiData.scala
+++ src/main/scala/quiz/KanjiData.scala
@@ -1,10 +1,17 @@
 package quiz
 
 import java.nio.file.Path
+import scala.reflect.ClassTag
 
-class KanjiData protected (path: Path) {
-  protected def loadLevels(path: Path): Map[String, Level.Value] = {
-    Level.defined.map(EnumListFile(path, _)).flatMap(f =>
+class KanjiData protected (val path: Path) {
+  private lazy val levels = load(Level)
+  private lazy val kyus = load(Kyu)
+
+  def level(s: String): Level.Value = levels.getOrElse(s, Level.None)
+  def kyu(s: String): Kyu.Value = kyus.getOrElse(s, Kyu.None)
+
+  private def load[T <: EnumWithNone: ClassTag](e: T): Map[String, T#Value] = {
+    e.defined.map(EnumListFile(path.resolve(e.toString), _)).flatMap(f =>
       f.entries.map(_ -> f.value)
     ).toMap
   }
--- src/test/scala/quiz/KanjiDataTest.scala
+++ src/test/scala/quiz/KanjiDataTest.scala
@@ -1,33 +1,29 @@
 package quiz
 
-import quiz.KanjiDataTest.TestKanjiData
-
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.Files
 
 class KanjiDataTest extends FileTest {
   override protected def afterEach(): Unit = {
     EnumListFile.clearEntryData()
+    super.afterEach()
   }
 
-  "loadLevels loads all Level files and returns a map of Kanji to Level" in {
-    Files.writeString(tempDir.resolve("N5.txt"), "一 二 三\n四 五 六")
-    Files.writeString(tempDir.resolve("N4.txt"), "七")
-    Files.writeString(tempDir.resolve("N3.txt"), "八")
-    Files.writeString(tempDir.resolve("N2.txt"), "九")
-    Files.writeString(tempDir.resolve("N1.txt"), "十")
-    val data = new TestKanjiData
-    val expected = (Seq("一", "二", "三", "四", "五", "六").map(_ -> Level.N5) :+
-      ("七" -> Level.N4) :+
-      ("八" -> Level.N3) :+
-      ("九" -> Level.N2) :+
-      ("十" -> Level.N1)).toMap
-    assert(data.loadLevels(tempDir) == expected)
-  }
-}
-
-object KanjiDataTest {
-  class TestKanjiData extends KanjiData(Paths.get("")) {
-    override def loadLevels(path: Path): Map[String, Level.Value] =
-      super.loadLevels(path)
+  "load[Level] loads all Level files and returns a map of Kanji to Level" in {
+    val p = tempDir.resolve("Level")
+    Files.createDirectory(p)
+    Files.writeString(p.resolve("N5.txt"), "一 二 三\n四 五 六")
+    Files.writeString(p.resolve("N4.txt"), "七")
+    Files.writeString(p.resolve("N3.txt"), "八")
+    Files.writeString(p.resolve("N2.txt"), "九")
+    Files.writeString(p.resolve("N1.txt"), "十")
+    val data = KanjiData(tempDir)
+    Seq("一", "二", "三", "四", "五", "六").foreach(s =>
+      assert(data.level(s) == Level.N5)
+    )
+    assert(data.level("七") == Level.N4)
+    assert(data.level("八") == Level.N3)
+    assert(data.level("九") == Level.N2)
+    assert(data.level("十") == Level.N1)
+    assert(data.level("百") == Level.None)
   }
 }
