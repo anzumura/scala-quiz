a9f204f75d0ab438d0964b29d0b31db25113a929
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-06 00:46:33 -0500

    add MorohashiId class

---
+++ src/main/scala/quiz/kanji/MorohashiId.scala
@@ -0,0 +1,52 @@
+package quiz.kanji
+
+import quiz.kanji.MorohashiId.IndexType
+import quiz.utils.ThrowsDomainException
+
+import scala.util.{Success, Try}
+
+class MorohashiId private (val index: Int, val indexType: IndexType) {
+  import IndexType._
+
+  override def toString: String = (indexType match {
+    case Plain => "%05d"
+    case Prime => "%05dP"
+    case DoublePrime => "%05dPP"
+    case Supplemental => "H%03d"
+  }).formatted(index)
+}
+
+object MorohashiId extends ThrowsDomainException {
+  val MaxIndex: Int = 99999
+  val MaxSupplementalIndex: Int = 999
+
+  sealed trait IndexType
+  object IndexType {
+    case object Plain extends IndexType
+    case object Prime extends IndexType
+    case object DoublePrime extends IndexType
+    case object Supplemental extends IndexType
+  }
+  import IndexType._
+
+  def apply(index: Int, indexType: IndexType = Plain): MorohashiId = {
+    if (index < 0) domainError("negative index")
+    (if (indexType == Supplemental) MaxSupplementalIndex else MaxIndex) match {
+      case x if index > x => domainError(s"$indexType index $index exceeds $x")
+      case _ => new MorohashiId(index, indexType)
+    }
+  }
+
+  def apply(id: String): MorohashiId = {
+    if (id.isEmpty) domainError("empty index")
+    val (index, indexType) =
+      if (id.startsWith("H")) (id.drop(1), Supplemental)
+      else if (id.endsWith("PP")) (id.dropRight(2), DoublePrime)
+      else if (id.endsWith("P")) (id.dropRight(1), Prime)
+      else (id, Plain)
+    Try(index.toInt) match {
+      case Success(x) => apply(x, indexType)
+      case _ => domainError(s"invalid format '$id'")
+    }
+  }
+}
---
+++ src/test/scala/quiz/kanji/MorohashiIdTest.scala
@@ -0,0 +1,118 @@
+package quiz.kanji
+
+import quiz.kanji.MorohashiId.IndexType._
+import quiz.kanji.MorohashiId.{MaxIndex, MaxSupplementalIndex}
+import quiz.utils.BaseTest
+
+class MorohashiIdTest extends BaseTest {
+  "create" - {
+    "plain id" in {
+      val m = MorohashiId(12345)
+      assert(m.index == 12345)
+      assert(m.indexType == Plain)
+    }
+
+    "prime id" in {
+      val m = MorohashiId(67890, Prime)
+      assert(m.index == 67890)
+      assert(m.indexType == Prime)
+    }
+
+    "double prime id" in {
+      val m = MorohashiId(45678, DoublePrime)
+      assert(m.index == 45678)
+      assert(m.indexType == DoublePrime)
+    }
+
+    "supplemental id" in {
+      val m = MorohashiId(789, Supplemental)
+      assert(m.index == 789)
+      assert(m.indexType == Supplemental)
+    }
+  }
+
+  "create from string" - {
+    "plain id" in {
+      val m = MorohashiId("12345")
+      assert(m.index == 12345)
+      assert(m.indexType == Plain)
+    }
+
+    "prime id" in {
+      val m = MorohashiId("67890P")
+      assert(m.index == 67890)
+      assert(m.indexType == Prime)
+    }
+
+    "double prime id" in {
+      val m = MorohashiId("45678PP")
+      assert(m.index == 45678)
+      assert(m.indexType == DoublePrime)
+    }
+
+    "supplemental id" in {
+      val m = MorohashiId("H789")
+      assert(m.index == 789)
+      assert(m.indexType == Supplemental)
+    }
+  }
+
+  "create error" - {
+    "negative index" in {
+      domainError(MorohashiId(-2), "negative index")
+    }
+
+    "index out of range" in {
+      val big = MaxIndex + 1
+      Seq(Plain, Prime, DoublePrime).foreach(i =>
+        domainError(MorohashiId(big, i), s"$i index $big exceeds $MaxIndex")
+      )
+    }
+
+    "supplemental index out of range" in {
+      val big = MaxSupplementalIndex + 1
+      domainError(MorohashiId(big, Supplemental),
+        s"$Supplemental index $big exceeds $MaxSupplementalIndex")
+    }
+
+    "empty index" in {
+      domainError(MorohashiId(""), "empty index")
+    }
+
+    "invalid format" in {
+      Seq("H123P", "123PPP", "PP", "H", "xyz").foreach(s =>
+        domainError(MorohashiId(s), s"invalid format '$s'")
+      )
+    }
+  }
+
+  "to string" - {
+    "plain index" in {
+      assert(MorohashiId(12345).toString == "12345")
+    }
+
+    "prime index has trailing 'P'" in {
+      assert(MorohashiId(67890, Prime).toString == "67890P")
+    }
+
+    "double prime index has trailing 'PP'" in {
+      assert(MorohashiId(34567, DoublePrime).toString == "34567PP")
+    }
+
+    "supplemental index has leading 'H'" in {
+      assert(MorohashiId(567, Supplemental).toString == "H567")
+    }
+
+    "add leading zeroes if less than 5 digits for non-Supplemental index" in {
+      Seq(Plain, Prime, DoublePrime).foreach { i =>
+        assert(MorohashiId(1, i).toString.startsWith("00001"))
+        assert(MorohashiId(1234, i).toString.startsWith("01234"))
+      }
+    }
+
+    "add leading zeroes if less than 3 digits for Supplemental index" in {
+      assert(MorohashiId(1, Supplemental).toString.startsWith("H001"))
+      assert(MorohashiId(12, Supplemental).toString.startsWith("H012"))
+    }
+  }
+}
