00b2aab0577c61dd5bc3486d427a51d07840fe19
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-11 22:44:25 -0500

    move main method to quiz.main package

--- src/main/scala/quiz/main/Quiz.scala
+++ src/main/scala/quiz/main/Quiz.scala
@@ -1,12 +1,12 @@
-package quiz
+package quiz.main
 
 import quiz.data.KanjiData
 import quiz.kanji.KanjiType
 
 import scala.util.Try
 
-object Quiz extends App {
-  Try {
+object Quiz {
+  def main(args: Array[String]): Unit = Try {
     val data = KanjiData(KanjiData.dataDir())
     KanjiType.values.foreach { t => println(s"Loaded ${data.getType(t).size} $t Kanji") }
   }.failed.foreach(e => println("got exception: " + e))
