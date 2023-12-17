47e1b96aeb83361a9ce8eb92ea7201805836860e
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-16 21:31:55 -0500

    cleanup and add scala docs

--- src/main/scala/quiz/Quiz.scala
+++ src/main/scala/quiz/Quiz.scala
@@ -23,50 +23,39 @@ class Quiz(data: KanjiData, choice: Choice, randomize: Boolean) {
       case x => !choice.isQuit(x)
     }) {}
 
-  private def frequency(): Boolean = {
-    Range('1', '9').get(choice, Map('0' -> "most frequent"), "Frequency buckets of 250") match {
-      case x if choice.isQuit(x) => false
-      case x =>
-        val pos = (x - '0') * FrequencyBlock
-        val end = (if (x == '9') data.frequencyList.size else pos + FrequencyBlock) + 1
-        makeList(data.frequencyList.slice(pos, end))
-    }
+  private def frequency(): Boolean = Range('1', '9').get(choice, FrequencyMap, FrequencyMsg) match {
+    case x if choice.isQuit(x) => false
+    case x =>
+      val pos = (x - '0') * FrequencyBlock
+      val end = (if (x == '9') data.frequencyList.size else pos + FrequencyBlock) + 1
+      makeList(data.frequencyList.slice(pos, end))
   }
 
-  private def grade(): Boolean = {
-    Range('1', '6').get(choice, Map('s' -> "Secondary School"), "Grade", 's') match {
-      case x if choice.isQuit(x) => false
-      case x => makeList(data.gradeMap(if (x == 's') Grade.S else Grade.fromOrdinal(x - '1')))
-    }
+  private def grade(): Boolean = Range('1', '6').get(choice, GradeMap, "Grade", 's') match {
+    case x if choice.isQuit(x) => false
+    case x => makeList(data.gradeMap(if (x == 's') Grade.S else Grade.fromOrdinal(x - '1')))
   }
 
-  private def level(): Boolean = {
-    choice.get(Range('1', '5'), "JLPT Level", '1') match {
-      case x if choice.isQuit(x) => false
-      case x => makeList(data.levelMap(Level.valueOf("N" + x)))
-    }
+  private def level(): Boolean = choice.get(Range('1', '5'), "JLPT Level", '1') match {
+    case x if choice.isQuit(x) => false
+    case x => makeList(data.levelMap(Level.valueOf("N" + x)))
   }
 
-  private def kyu(): Boolean = {
-    Range('1', '9')
-      .get(choice, Map('0' -> "k10", 'j' -> "Jun-K2", 'k' -> "Jun-K1"), "Kentei Kyu") match {
-      case x if choice.isQuit(x) => false
-      case x => makeList(data.kyuMap(x match {
-          case '0' => Kyu.K10
-          case 'j' => Kyu.KJ2
-          case 'k' => Kyu.KJ1
-          case _ => Kyu.valueOf("K" + x)
-        }))
-    }
+  private def kyu(): Boolean = Range('1', '9').get(choice, KyuMap, "Kentei Kyu") match {
+    case x if choice.isQuit(x) => false
+    case x => makeList(data.kyuMap(x match {
+        case '0' => Kyu.K10
+        case 'j' => Kyu.KJ2
+        case 'k' => Kyu.KJ1
+        case _ => Kyu.valueOf("K" + x)
+      }))
   }
 
-  private def makeList(entries: Vector[Kanji]) = {
-    choice.get(ListOrder, "List order", 'r') match {
-      case FromBeginning => run(entries)
-      case FromEnd => run(entries.reverse)
-      case RandomOrder => run(random.shuffle(entries))
-      case _ => false
-    }
+  private def makeList(entries: Vector[Kanji]) = choice.get(ListOrder, "List order", 'r') match {
+    case FromBeginning => run(entries)
+    case FromEnd => run(entries.reverse)
+    case RandomOrder => run(random.shuffle(entries))
+    case _ => false
   }
 
   private def run(entries: Vector[Kanji]) = {
@@ -79,7 +68,7 @@ class Quiz(data: KanjiData, choice: Choice, randomize: Boolean) {
   }
 
   private def question(entries: Vector[Kanji], k: Kanji, result: Result) = {
-    val choices = mutable.Set[Int](result.question)
+    val choices = mutable.Set(result.question)
     while (choices.size < 4) choices.add(Random.nextInt(entries.size))
     val answers = choices.toArray
 
@@ -119,27 +108,46 @@ object Quiz {
     Map(FromBeginning -> "from beginning", FromEnd -> "from end", RandomOrder -> "random")
   // Frequency Quiz
   private val FrequencyBlock = 250
+  private val FrequencyMap = Map('0' -> "most frequent")
+  private val FrequencyMsg = s"Frequency buckets of $FrequencyBlock"
+
+  private val GradeMap = Map('s' -> "Secondary School")
+  private val KyuMap = Map('0' -> "k10", 'j' -> "Jun-K2", 'k' -> "Jun-K1")
 
   private enum State {
     case MeaningOff, MeaningOn, Quit
   }
   import State.*
   private case class Result(question: Int = 0, score: Int = 0, state: State = MeaningOff) {
+    /** return string representation of this Result that includes the current score and question */
     override def toString: String = s"$score/$question"
 
+    /** return a message for a quiz question that includes the question number, current score and
+     *  Kanji being tested (optionally followed by the Kanji meaning depending on `state`)
+     */
     def questionMsg(size: Int, k: Kanji): String =
       s"\nQuestion ${question + 1} of $size (score $score/${question - score}): Kanji ${k.name}" +
         (if (state == MeaningOn) s" - ${k.meaning}" else "")
+
+    /** return a message for flipping meaning based on the current value of `state` */
     def meaningMsg: String = (if (state == MeaningOn) "hide" else "show") + " meanings"
 
+    /** return a Result instance for the next question
+     *  @param correct if true then increment `score`
+     */
     def next(correct: Boolean): Result =
       Result(question + 1, if (correct) score + 1 else score, state)
 
+    /** return a Result instance with 'meaning' state flipped, i.e., if state is `MeaningOn`
+     *  the set it to `MeaningOff` and vice versa
+     */
     def flipMeaning: Result =
       Result(question, score, if (state == MeaningOn) MeaningOff else MeaningOn)
 
+    /** return a Result instance with state set to `Quit` */
     def quit: Result = Result(question, score, Quit)
 
+    /** return true if state is `Quit` */
     def isQuit: Boolean = state == Quit
   }
 }
