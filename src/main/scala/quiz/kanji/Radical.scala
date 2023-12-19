fd945b0757b420a1132d496126a247161bef2ebb
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-18 20:58:55 -0500

    add some validation to Radical class

--- src/main/scala/quiz/kanji/Radical.scala
+++ src/main/scala/quiz/kanji/Radical.scala
@@ -1,4 +1,26 @@
 package quiz.kanji
 
-case class Radical(number: Int, name: String, altNames: List[String], longName: String,
-    reading: String) {}
+import quiz.kanji.Radical.MaxRadical
+import quiz.utils.Block.isOneKanji
+import quiz.utils.ThrowsDomainException
+
+final class Radical(val number: Int, val name: String, val altNames: List[String],
+    val longName: String, val reading: String)
+extends ThrowsDomainException {
+  if (number < 1 || number > MaxRadical) error(
+    s"Radical number $number not between 1 and $MaxRadical")
+  if (!isOneKanji(name)) error(s"Radical name '$name' is not a single Kanji")
+  altNames.foreach(x => if (!isOneKanji(x)) error(s"Radical altName '$x' is not a single Kanji"))
+
+  override def toString: String = s"$name($number)"
+  override def equals(obj: Any): Boolean = obj match {
+    case that: Radical => number == that.number
+    case _ => false
+  }
+
+  override def hashCode(): Int = number.##
+}
+
+object Radical {
+  val MaxRadical = 214
+}
---
+++ src/test/scala/quiz/kanji/RadicalTest.scala
@@ -0,0 +1,37 @@
+package quiz.kanji
+
+import quiz.test.BaseTest
+import quiz.test.BaseTest.testRadical
+
+class RadicalTest extends BaseTest {
+  "toString returns name followed by number in brackets" in {
+    assert(testRadical.toString == testRadical.name + s"(${testRadical.number})")
+  }
+
+  private val rad1a = Radical(1, "一", Nil, "", "")
+  private val rad1b = Radical(1, "二", Nil, "", "")
+  private val rad2 = Radical(2, "二", Nil, "", "")
+
+  "equals is only based on Radical number" in {
+    // when real data is loaded there won't be two 'different' radicals with the same number
+    assert(rad1a == rad1b)
+    assert(rad1a != rad2)
+    assert(rad1b != rad2)
+  }
+
+  "hashCode is only based on Radical number" in {
+    assert(rad1a.hashCode == rad1b.hashCode)
+    assert(rad1a.hashCode != rad2.hashCode)
+    assert(rad1b.hashCode != rad2.hashCode)
+  }
+
+  "number must be between 1 and 214" in Seq(-1, 0, 215).foreach(number =>
+    error(Radical(number, "三", Nil, "", ""), s"Radical number $number not between 1 and 214"))
+
+  "name must be a single Kanji" in Seq("二三", "A", "ア", "あ").foreach(name =>
+    error(Radical(1, name, Nil, "", ""), s"Radical name '$name' is not a single Kanji"))
+
+  "each alternative name must be a single Kanji" in Seq("二三", "A", "ア", "あ").foreach(altName =>
+    error(Radical(1, "一", List("二", altName, "三"), "", ""),
+      s"Radical altName '$altName' is not a single Kanji"))
+}
