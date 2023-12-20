8a52e5306f1457320966820fa0a3c12791004f6e
Dan H <9552296+anzumura@users.noreply.github.com>
2023-12-19 20:54:26 -0500

    print list of mistakes

--- src/main/scala/quiz/Quiz.scala
+++ src/main/scala/quiz/Quiz.scala
@@ -62,35 +62,38 @@ class Quiz(data: KanjiData, choice: Choice, randomize: Boolean) {
     }
 
   private def run(entries: Vector[Kanji], exclude: Info) = {
-    val result = entries.foldLeft(Result()) {
+    val state = entries.foldLeft(State(exclude)) {
       case (r, _) if r.isQuit => r // once quit has been set don't modify result anymore
-      case (r, k) => question(entries, k, r, exclude)
+      case (r, k) => question(entries, k, r)
     }
-    choice.println(s"\n>>> Final score: $result\n")
-    !result.isQuit
+    choice.println(s"\n>>> Final score: $state\n")
+    !state.isQuit
   }
 
-  private def question(entries: Vector[Kanji], k: Kanji, result: Result, exclude: Info) = {
+  private def question(entries: Vector[Kanji], k: Kanji, state: State) = {
     // create array of answers that contains the correct answer plus 3 other answers with different
     // readings - use TreeMap so that answers are sorted in 'reading' order
-    val answers = Iterator.iterate(TreeMap(k.reading -> result.question)) { m =>
+    val answers = Iterator.iterate(TreeMap(k.reading -> state.question)) { m =>
       val answer = Random.nextInt(entries.size)
       m + (entries(answer).reading -> answer)
     }.dropWhile(_.size < ChoicesPerQuestion).next().values.toArray
 
     @tailrec
-    def f(r: Result): Result = {
-      choice.println(r.questionMsg(entries.size, k, exclude))
+    def f(s: State): State = {
+      choice.println(s.questionMsg(entries.size, k))
       answers.zipWithIndex.foreach { case (answer, index) =>
         choice.println(s"  ${index + 1}: " + entries(answer).reading)
       }
-      QuestionRange.get(choice, Map(FlipMeaning -> r.meaningMsg), "Choose reading") match {
-        case FlipMeaning => f(r.flipMeaning)
-        case x if choice.isQuit(x) => r.quit
-        case x => r.next(answers(x - '1') == r.question)
+      QuestionRange.get(choice, Map(FlipMeaning -> s.meaningMsg), "Choose reading") match {
+        case FlipMeaning => f(s.flipMeaning)
+        case x if choice.isQuit(x) => s.quit
+        case x if answers(x - '1') == s.question => s.correct
+        case _ =>
+          choice.println(s"  Incorrect: the answer is ${answers.indexOf(s.question) + 1}")
+          s.incorrect(k.name)
       }
     }
-    f(result)
+    f(state)
   }
 }
 
@@ -129,40 +132,52 @@ object Quiz {
   private val KJ1 = 'k'
   private val KyuMap = Map(K10 -> "k10", KJ2 -> "Jun-K2", KJ1 -> "Jun-K1")
 
-  private enum State {
-    case MeaningOff, MeaningOn, Quit
+  private object State {
+    def apply(exclude: Info): State = new State(exclude)
+
+    enum States {
+      case MeaningOff, MeaningOn, Quit
+    }
   }
-  import State.*
-  private case class Result(question: Int = 0, score: Int = 0, state: State = MeaningOff) {
-    /** return string representation of this Result that includes the current score and question */
-    override def toString: String = s"$score/$question"
+  import State.States
+  import State.States.*
+  private final class State private (exclude: Info, val question: Int = 0,
+      mistakes: Vector[String] = Vector[String](), state: States = States.MeaningOff) {
+    /** return string that includes the current score and list of mistakes */
+    override def toString: String = s"$score/${if (state == Quit) question else question + 1}" +
+      mistakes.mkString(" mistakes: ", ", ", "")
 
     /** return a message for a quiz question that includes the question number, current score and
      *  Kanji being tested (optionally followed by the Kanji meaning depending on `state`)
      */
-    def questionMsg(size: Int, k: Kanji, exclude: Info): String =
-      s"\nQuestion ${question + 1} of $size (score $score/${question - score}): Kanji " +
-        k.info(exclude) + (if (state == MeaningOn) s" - ${k.meaning}" else "")
+    def questionMsg(size: Int, k: Kanji): String =
+      s"\nQuestion ${question + 1} of $size (score $score): Kanji " + k.info(exclude) +
+        (if (state == MeaningOn) s" - ${k.meaning}" else "")
 
     /** return a message for flipping meaning based on the current value of `state` */
     def meaningMsg: String = (if (state == MeaningOn) "hide" else "show") + " meanings"
 
-    /** return a Result instance for the next question
-     *  @param correct if true then increment `score`
+    /** return a State instance for the next question */
+    def correct: State = new State(exclude, question + 1, mistakes, state)
+
+    /** return a State instance for the next question
+     *  @param mistake the Kanji for the current question that was answered incorrectly
      */
-    def next(correct: Boolean): Result =
-      Result(question + 1, if (correct) score + 1 else score, state)
+    def incorrect(mistake: String): State =
+      new State(exclude, question + 1, mistakes :+ mistake, state)
 
-    /** return a Result instance with 'meaning' state flipped, i.e., if state is `MeaningOn`
-     *  the set it to `MeaningOff` and vice versa
+    /** return a copy of this State instance with 'meaning' flipped, i.e., if state is `MeaningOn`
+     *  then set it to `MeaningOff` and vice versa
      */
-    def flipMeaning: Result =
-      Result(question, score, if (state == MeaningOn) MeaningOff else MeaningOn)
+    def flipMeaning: State =
+      new State(exclude, question, mistakes, if (state == MeaningOn) MeaningOff else MeaningOn)
 
-    /** return a Result instance with state set to `Quit` */
-    def quit: Result = Result(question, score, Quit)
+    /** return a copy of this State instance with state set to `Quit` */
+    def quit: State = new State(exclude, question, mistakes, Quit)
 
-    /** return true if state is `Quit` */
+    /** return true if `state` is `Quit` */
     def isQuit: Boolean = state == Quit
+
+    private def score = question - mistakes.size
   }
 }
